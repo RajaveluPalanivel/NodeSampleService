eee69ff8ef7c7cdaa88172d983e8543eea2ac8fc
Rajavelu Palanivel <Rajavelu Palanivel@LAPTOP-J2APTBBL>
2022-01-29 10:29:47 +0530

    test

---
+++ index.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const server = express();
+const shortId = require('shortid');
+
+const Request = require('request');
+
+const PORT = 5000;
+
+server.use(express.json());
+
+const channels = []
+const lessons = []
+
+
+
+server.get('/', function (req, res) {
+    res.json({ Hello: "Raja" });
+});
+
+server.get('/api/channels', function (req, res) {
+    res.status(200).json(channels);
+});
+
+server.listen(5000, () => {
+    console.log('\n*** Server running on http://localhost:' + PORT);
+});
+
+server.post('/api/channels', async function (req, resOut) {
+   
+    console.log("Body:" + JSON.stringify(req.body));
+    Request.post({
+        "headers": {
+            "Content-Type": "application/json",
+            "api-key": "WXNDC2OfhluWCsmpjsjrX5uA7nA16PPIy3h5ty7X83B4SFutHpHjTVLjNw21SkU8"
+        },
+        "url": "https://data.mongodb-api.com/app/data-mlytw/endpoint/data/beta/action/insertOne",
+        "body": JSON.stringify(req.body)
+    }, (error, response, body) => {
+        if (error) { console.log("Error message is: " + error) }
+        else {
+            console.log("The Response is: " + JSON.stringify(body));
+            resOut.status(201).json(body);
+        }
+    });
+
+    resOut.status(201).json(channelInfo);
+});
